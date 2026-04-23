ded48325cd74eabcd71c3b30d213a306a9e4ee7c
Teté <elizabethcornejo@liceovvh.cl>
2026-04-23 14:22:54 -0400

    tarea terminada

--- python/core/funciones_intermedias_(core)_Elizabeth_Cornejo_04_03_2026/funciones_intermedias.py
+++ python/core/funciones_intermedias_(core)_Elizabeth_Cornejo_04_03_2026/funciones_intermedias.py
@@ -1,12 +1,21 @@
+#parte 1
+
 # Ranking de puntajes de un torneo de eSports
-puntajes = [ [1000, 1500, 2000], [300, 700, 1400] ]
+puntaje = [ [1000, 1500, 2000], [300, 700, 1400] ]
+
+# Ranking de puntajes de un torneo de eSports
+puntaje = [ [1000, 1500, 2000], [300, 700, 1400] ]
+puntaje[1][0] = 600
+print(puntaje)
 
 
 # Lista de creadores de contenido en una plataforma de streaming
-streamers = [
+streamer = [
     {"nombre": "GameNinjaPro", "seguidores": 250000},
     {"nombre": "PixelWarrior", "seguidores": 180000}
 ]
+streamer[0]["nombre"] = "EliteGamerX"
+print(streamer)
 
 
 # Eventos en distintas ciudades del mundo
@@ -15,22 +24,39 @@ eventos = {
     "España": ["Madrid", "Barcelona", "Valencia"]
 }
 
+eventos["Estados Unidos"][2] = "San Francisco"
+print(eventos)
+
 
 # Coordenadas de la sede de un torneo internacional
 ubicacion = [
     {"latitud": 34.052235, "longitud": -118.243683}
 ]
 
+ubicacion[0]["latitud"] = "40.712776"
+print(ubicacion)
 
-# En puntajes, cambia el valor 300 por 600 (ajuste en los puntajes de la primera ronda). Resultado esperado:
-# puntajes = [[1000, 1500, 2000], [600, 700, 1400]]​​
-puntajes[1][0] = 600
-print(puntajes)
-# En streamers, cambia el nombre de ”GameNinjaPro” a ”EliteGamerX”.
-streamers[0]["nombre"] = "EliteGamerX"
-print(streamers)
-# En eventos, cambia la ciudad “Las Vegas” por “San Francisco”.
-eventos["Estados Unidos"][2] = "San Francisco"
-print(eventos)
-# En ubicacion, cambia el valo de ”latitud” a 40.712776 (cambiando la sede del torneo a Nueva York)
-ubicacion [0] ["latitud"]  
+#parte 2
+
+def diccionario(lista):
+    for streamer in lista:
+        print(f"nombre : {streamer['nombre']} seguidores : {streamer['seguidores']}")
+
+
+diccionario(streamer)
+
+def valores(clave, lista):
+    for item in lista:
+        print(item[clave])
+
+valores("nombre", streamer)
+valores("seguidores", streamer)
+
+
+#parte 3
+
+
+for paises, lista in eventos.items():
+    print(f"---{paises.upper()}---")
+    for item in lista:
+        print(f"{item}")
